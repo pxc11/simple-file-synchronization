39b5024ae769ccc65677ff26ece7893edad80d35
peter <1184558669@qq.com>
2024-04-26 21:59:14 +0800

    optimization

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # simple-file-synchronization
+## 双向文件同步程序
+<br>
+<br>
 Simple two-way  file synchronization used to sync file between remote and local.
-# how use
-Config in server.js and client.js.  
+
+## how use
+rename [config.js.example](config.js.example) to [config.js](config.js) and config it.
 Then execute:  
 `node server.js`  in remote.  
 `node client.js`  in local.  
--- client.js
+++ client.js
@@ -1,31 +1,15 @@
 const net = require('net');
 const client = new net.Socket();
 const common = require('./lib/common.js');
-const path = require("path");
-
-const PORT = 3000;
-const HOST = '127.0.0.1';
-const WATCH_PATH = path.join(__dirname, '../test/client');
-const LISTEN_PATH = WATCH_PATH;
-const OPTIONS = {
-    ignored: [
-
-    ],
-    ignoreInitial: true,
-
-};
-
+const {PORT, HOST, WATCH_PATH, LISTEN_PATH, OPTIONS, WATCH_INTERVAL_TIME} = require('./config.js').clientConfig;
 // 连接到服务器
 client.connect(PORT, HOST, () => {
     console.log('Connected to server');
     common.listenFileChange(client, LISTEN_PATH);
-    common.watchFileChange(WATCH_PATH, OPTIONS, client);
+    common.watchFileChange(WATCH_PATH, OPTIONS, client, WATCH_INTERVAL_TIME);
 
 });
 
 client.on('close', () => {
     console.log('Connection closed');
 });
-
-
-
