a00c378cf7ea5d47f81b4fba800aae004b67cb74
peter <1184558669@qq.com>
2024-07-17 00:18:01 +0800

    2.0

--- client.js
+++ client.js
@@ -1,20 +1,3 @@
-const net = require('net');
-const client = new net.Socket();
-const common = require('./lib/common.js');
-const {PORT, HOST, WATCH_PATH, LISTEN_PATH, OPTIONS, WATCH_INTERVAL_TIME} = require('./config.js').clientConfig;
-// 连接到服务器
-client.connect(PORT, HOST, () => {
-    console.log('Connected to server');
-    common.listenFileChange(client, LISTEN_PATH);
-    common.watchFileChange(WATCH_PATH, OPTIONS, client, WATCH_INTERVAL_TIME);
+const Client = require('./lib/Client');
 
-});
-
-client.on('error', (e) => {
-    console.log(e);
-    process.exit();
-})
-client.on('close', () => {
-    console.log('Connection closed');
-    process.exit();
-});
+const client = new Client();
--- server.js
+++ server.js
@@ -1,25 +1,4 @@
-const net = require('net');
-const common = require('./lib/common.js');
-const path = require('path');
+const Server = require('./lib/Server');
 
-const {PORT, WATCH_PATH, LISTEN_PATH, OPTIONS, WATCH_INTERVAL_TIME} = require('./config.js').serverConfig;
-
-
-const server = net.createServer((socket) => {
-    console.log('Client connected');
-    common.listenFileChange(socket, LISTEN_PATH);
-    common.watchFileChange(WATCH_PATH, OPTIONS, socket, WATCH_INTERVAL_TIME);
-
-    socket.on('end', () => {
-        console.log('Bye!');
-    });
-
-    socket.on('error', (error) => {
-        console.error('Socket error:', error);
-        socket.destroy();
-    });
-});
-
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+const server = new Server();
+server.listen();
